8f204b2fd13e8fdf3929d58f7e931fba3501b220
Rikka <rikka@xing.moe>
2016-03-08 13:51:40 +0800

    Add AboutActivity

--- app/src/main/java/rikka/akashitoolkit/ui/AboutActivity.java
+++ app/src/main/java/rikka/akashitoolkit/ui/AboutActivity.java
@@ -1,8 +1,10 @@
 package rikka.akashitoolkit.ui;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -16,6 +18,11 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
