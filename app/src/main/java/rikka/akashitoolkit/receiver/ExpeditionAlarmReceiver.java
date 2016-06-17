a40f0038451d6d30b84ff4ef8b7feadbbb047760
Rikka <rikka@xing.moe>
2016-06-17 23:52:48 +0800

    feat(notification): notification behavior change
    Add vibrate, sound and light

--- app/src/main/java/rikka/akashitoolkit/receiver/ExpeditionAlarmReceiver.java
+++ app/src/main/java/rikka/akashitoolkit/receiver/ExpeditionAlarmReceiver.java
@@ -1,11 +1,11 @@
 package rikka.akashitoolkit.receiver;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 
@@ -34,11 +34,9 @@ public class ExpeditionAlarmReceiver extends BroadcastReceiver {
                     .setContentText(context.getString(R.string.expedition_finished))
                     .setContentTitle(expedition.getName().get(context))
                     .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND)
+                    .setLights(ContextCompat.getColor(context, R.color.material_pink_500), 1000, 1000)
                     .setAutoCancel(true);
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                notificationBuilder.setVibrate(new long[0]);
-            }
 
             NotificationManager notificationManager =
                     (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
